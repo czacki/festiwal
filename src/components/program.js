b4fc4ebf3ab71eef45b5a6da2358fb38c00d4aa6
Krzysztof Chorzempa <chorzempa.krzysztof@gmail.com>
2020-10-10 20:20:16 +0200

    Simplified, english names

--- src/components/program.js
+++ src/components/program.js
@@ -1,96 +1,56 @@
 //"jak coś jest głupie, ale działa, to nie jest głupie" + jeszcze tak dobrze nie ogarniam Reacta xd
 import React, {useState, useEffect} from 'react'
-let data
+let data//TODO wziąć z bazy
 try
 {
-  data=require("../assets/sztuki")
+  data=require("../assets/plays")
 }
 catch(e)
 {
-  data={"start": [2020, 11, 2], "days": ["Środa", "Czwartek", "Piątek", "Sobota"], "sztuki": []}
+  data={"start": [2020, 11, 2], "days": ["Środa", "Czwartek", "Piątek", "Sobota"], "plays": []}
 }
-let festiwalStart=new Date(...data.start).getTime()
-let dayMs=3600*24*1000
+
+let festivalStart=new Date(...data.start).getTime()
+const hourMs=3600*1000
+const dayMs=hourMs*24
+const maxPlayLength=5/6*hourMs
 export default function Program()
 {
-  let days=data.days
-  let sztuki=new Array(days.length)
-  sztuki.fill([])
-  for(let sztuka of data.sztuki)
+  let days=data.days//nazwy dni festiwalu
+  let realDay=Math.floor((Date.now()-festivalStart)/dayMs)//teraz-start festiwalu w dniach
+  const [day, showDay]=useState(Math.min(days.length-1, Math.max(0, realDay)))//który dzień jest teraz pokazywany
+  let plays=data.plays
+  //sztuki muszą być posortowane
+  for(let i=0; i<plays.length; ++i)
   {
-    let [hour, minutes]=sztuka.oclock.split(":")
-    sztuka.timestamp=festiwalStart+sztuka.day*dayMs+parseInt(hour)*3600*1000+parseInt(minutes)*60000
-    sztuki[sztuka.day]=[...sztuki[sztuka.day], sztuka]//czemu nie działa push?
+    plays[i].id=i
+    //przetwarzam godzinę i dzień sztuki na timestamp (bo łatwiej do danych wklepać tylko godzinę i dzień)
+    let [hour, minutes]=plays[i].oclock.split(":")
+    plays[i].timestamp=festivalStart+plays[i].day*dayMs+parseInt(hour)*hourMs+parseInt(minutes)*60000
   }
 
-  let now=new Date()
-  let realDay=Math.floor((Date.now()-festiwalStart)/dayMs)
-  const [day, setDay]=useState(Math.min(3, Math.max(realDay, 0)))
-  const [playNow, updatePlay]=useState(-1)
-  let isFestiwal=(day===realDay)
-  function updateDay()
-  {
-    realDay=Math.floor((Date.now()-festiwalStart)/dayMs)
-    setDay(Math.min(3, Math.max(realDay, 0)))
-    setPlayNumber()
-    now=new Date()
-    setTimeout(updateDay, (new Date().setHours(24, 0, 0, 0)-new Date()))
-  }
-  useEffect(updateDay, [])
+  //która sztuka teraz trwa?
+  const [playNow, newPlayNow]=useState(-1)
 
-  //czy aktualizować automatycznie jak się zmieni dzień?
-
-  function nextPlay()
+  function init()
   {
-    now=new Date()
-    if(playNow===sztuki[realDay].length-1||playNow===-1)
+    let now=Date.now()
+    for(let i=0; i<plays.length; ++i)
     {
-      updatePlay(-1)
-      return
+      if(plays[i].timestamp-now>0)setTimeout(()=>{newPlayNow(plays[i].id)}, plays[i].timestamp-now)//początki sztuk
+      if(i<plays.length-1&&plays[i+1].timestamp<plays[i].timestamp+maxPlayLength)continue//jeśli następna sztuka zaczyna się wcześniej niż maksymalna długość sztuki to już nic nie rób
+      if(plays[i].timestamp+maxPlayLength-now>0)setTimeout(()=>{newPlayNow(-1)}, plays[i].timestamp+maxPlayLength-now)//przerwy
     }
-    updatePlay(playNow+1)
-    if(playNow===sztuki[realDay].length-2)setTimeout(nextPlay, sztuki[realDay].slice(-1)[0].timestamp+3600*1000-now)
-    else setTimeout(nextPlay, sztuki[realDay][playNow+1].timestamp-now)
+    let p=plays.findIndex(x=>x.timestamp<=now&&now<x.timestamp+maxPlayLength)
+    newPlayNow(p)
   }
-
-  function setPlayNumber()
-  {
-    if(!isFestiwal)
-    {
-      updatePlay(-1)
-      return
-    }
-    let tplays=sztuki[realDay]
-    if(tplays.length===0)
-    {
-      updatePlay(-1)
-      return
-    }
-    now=new Date()
-    if(now<tplays[0].timestamp)
-    {
-      updatePlay(-1)
-      setTimeout(nextPlay, tplays[0].timestamp-now)
-      return
-    }
-    if(now>tplays.slice(-1)[0].timestamp+3600*1000)
-    {
-      updatePlay(-1)
-      return
-    }
-    let p=tplays.findIndex(x=>x.timestamp>now)
-    if(p===-1)p=tplays.length
-    --p
-    updatePlay(p)
-    if(p===tplays.length-1)setTimeout(nextPlay, tplays.slice(-1)[0].timestamp+3600*1000-now)
-    else setTimeout(nextPlay, tplays[p+1].timestamp-now)
-  }
+  useEffect(init, [])
   return (
     <div>
       {
         days.map((d, i)=>
         (
-          <div key={d} className={i===day ? "activeDay" : null} onClick={()=>setDay(i)}>{d}</div>
+          <div key={d} className={i===day ? "activeDay" : null} onClick={()=>showDay(i)}>{d}</div>
         ))
       }
       <table>
@@ -101,9 +61,9 @@ export default function Program()
             <th>Klasa</th>
           </tr>
           {
-            sztuki[day].map((s, i)=>
+            plays.filter(x=>x.day===day).map((s, i)=>
             (
-              <tr key={`${s.name}${s.class}`} className={i===playNow&&day===realDay ? "now" : null}>
+              <tr key={`s${s.id}`} className={s.id===playNow&&day===realDay ? "now" : null}>
                 <td>{s.oclock}</td>
                 <td>{s.name}</td>
                 <td>{s.class}</td>
